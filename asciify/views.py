4711a37bdc24379b969d6df284bb4063e3b553de
Bill Song <songchao456@gmail.com>
2021-01-07 12:10:52 -0500

    add routes

--- asciify/views.py
+++ asciify/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>FilterMe Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>FilterMe About</h1>')
