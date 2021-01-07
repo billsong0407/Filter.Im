8a2562ee78e692909745d74b02d4c74c0e979d8f
Bill Song <songchao456@gmail.com>
2021-01-07 17:30:35 -0500

    add user registration content

--- FilterMe/urls.py
+++ FilterMe/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from users import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_view.register, name="register"),
     path('', include('asciify.urls')),
 ]
