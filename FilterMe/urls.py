4d21efb4303736c308583d2ee2e07acd676287a1
Bill Song <songchao456@gmail.com>
2021-01-07 18:11:59 -0500

    add user log in/out page

--- FilterMe/urls.py
+++ FilterMe/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_view
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_view.register, name="register"),
+    path('profile/', user_view.profile, name="profile"),
+    path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path('', include('asciify.urls')),
 ]
